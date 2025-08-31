50f54226a7c15c89c3144225651ca96340d537be
avEggHead <usa.pants45@gmail.com>
2025-08-31 15:36:00 -0400

    switching login requirements to username instead of email

--- StockPriceCloseUp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ StockPriceCloseUp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -75,10 +75,8 @@ namespace StockPriceCloseUp.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [EmailAddress]
-            [Display(Name = "Email")]
-            public string Email { get; set; }
-
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -114,8 +112,7 @@ namespace StockPriceCloseUp.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.UserName, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
